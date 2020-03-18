fa794afbad864afec4aef3dd4872e3d90717248b
Avinash Mittapalli <thenash@gmail.com>
2020-03-18 15:47:33 -0400

    g

---
+++ geo.js
@@ -0,0 +1,73 @@
+var map, infobox, covidLayer, synhLayer;
+var synhData = 'https://raw.githubusercontent.com/thenash/misc/master/synhlocations.geojson';
+var covidData = 'https://raw.githubusercontent.com/thenash/misc/master/covid.geojson';
+
+function GetMap() {
+    map = new Microsoft.Maps.Map('#myMap', {
+        zoom: 2
+    });
+
+    //Create an infobox.
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        visible: false
+    });
+    infobox.setMap(map);
+
+
+    //Create a layer to add the GeoJSON data to.
+    covidLayer = new Microsoft.Maps.Layer();
+    synhLayer = new Microsoft.Maps.Layer();
+    map.layers.insert(covidLayer);
+    map.layers.insert(synhLayer);
+
+    //Add click event to the layer.
+    Microsoft.Maps.Events.addHandler(covidLayer, 'click', showInfobox);
+    Microsoft.Maps.Events.addHandler(synhLayer, 'click', showInfobox2);
+
+    //Load the GeoJSON module and read the GeoJSON feed.
+    Microsoft.Maps.loadModule('Microsoft.Maps.GeoJson', function() {
+        Microsoft.Maps.GeoJson.readFromUrl(covidData, function(shapes) {
+            //Add shapes to the layer.
+            covidLayer.add(shapes);
+        });
+        Microsoft.Maps.GeoJson.readFromUrl(synhData, function(shapes) {
+            //Add shapes to the layer.
+            synhLayer.add(shapes);
+        });
+
+    });
+}
+
+function showInfobox(e) {
+    var shape = e.target; //Get the clicked shape.
+    var loc = e.location; //Default to the location of the mouse event to show the infobox.
+
+    //If the shape is a pushpin, use it's location to display the infobox.
+    if (shape instanceof Microsoft.Maps.Pushpin) {
+        loc = shape.getLocation();
+    }
+
+    //Display the infoboc
+    infobox.setOptions({
+        location: loc,
+        title: shape.metadata.title + "\n" + shape.metadata.confirmed + "\n" + shape.metadata.deaths + "\n" + shape.metadata.recovered,
+        visible: true
+    });
+}
+
+function showInfobox2(e) {
+    var shape = e.target; //Get the clicked shape.
+    var loc = e.location; //Default to the location of the mouse event to show the infobox.
+
+    //If the shape is a pushpin, use it's location to display the infobox.
+    if (shape instanceof Microsoft.Maps.Pushpin) {
+        loc = shape.getLocation();
+    }
+
+    //Display the infoboc
+    infobox.setOptions({
+        location: loc,
+        title: shape.metadata.office_address,
+        visible: true
+    });
+}
